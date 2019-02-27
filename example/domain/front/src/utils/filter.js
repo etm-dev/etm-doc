71bb0b18ba97b745d9f8d021529c3927f46d0714
ray <364664251@qq.com>
2019-02-27 10:18:26 +0800

    删除无用文件

--- example/domain/front/src/utils/filter.js
+++
@@ -1,9 +0,0 @@
-let filter = function (text, length, clamp) {
-  clamp = clamp || '...'
-  var node = document.createElement('div')
-  node.innerHTML = text
-  var content = node.textContent
-  return content.length > length ? content.slice(0, length) + clamp : content
-}
-
-export default filter
